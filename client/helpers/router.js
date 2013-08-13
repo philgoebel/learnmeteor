8f6aecac77c96e16435309b852e5968d577e3ba1
Phil Goebel <philgoebel@gmail.com>
2013-08-13 21:20:24 +1000

    added submit post page

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -1,7 +1,10 @@
 Meteor.Router.add({
     '/': 'postsList',
+
     '/posts/:_id': {
 	to: 'postPage',
 	and: function(id) { Session.set('currentPostId', id); }
-    }
+    },
+
+    '/submit': 'postSubmit'
 });
